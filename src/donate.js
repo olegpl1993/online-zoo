4dc83edde131d32c537584dd9068ffc293bdff32
olegpl1993 <olegpl1993@gmail.com>
2022-10-15 00:40:10 +0300

    deploy gh-pages

--- src/donate.js
+++
@@ -1,6 +0,0 @@
-import './styles/global.scss';
-import { burger } from './js/burger';
-import { donate } from './js/donateMoney';
-
-burger(); //подключение бургер меню
-donate(); //выбор суммы доната
